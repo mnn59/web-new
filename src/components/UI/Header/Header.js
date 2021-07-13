c86bf3e4170f16ca76f0d947c13bcfc5247b449b
mahdi niknejad <m.niknejad@aut.ac.ir>
2021-07-13 11:49:43 -0700

    Add Header

---
+++ src/components/UI/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from "./Header.module.css"
+
+const header = () => {
+    return (
+        <div className={classes.Header}>
+
+        </div>
+    )
+}
+
+export default header
