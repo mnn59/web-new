82e0bee168824f8286e03f4d8178287062b558e6
mahdi niknejad <m.niknejad@aut.ac.ir>
2021-07-14 00:30:26 -0700

    Implement Header

--- src/components/UI/Header/Header.js
+++ src/components/UI/Header/Header.js
@@ -1,10 +1,30 @@
 import React from "react";
 import classes from "./Header.module.css"
+import LoginButton from "../LoginButton/LoginButton"
 
 const header = () => {
     return (
         <div className={classes.Header}>
-
+            <LoginButton value="ورود / ثبت نام"/>
+            <div className={classes.Navbar}>
+                <ul className={classes.menu__row}>
+                    <li className={classes.menu__list}>
+                        <a href="#" className={classes.menu__link}>محصولات</a>
+                    </li>
+                    <li className={classes.menu__list}>
+                        <a href="#" className={classes.menu__link}>پشتیبانی</a>
+                    </li>
+                    <li className={classes.menu__list}>
+                        <a href="#" className={classes.menu__link}>تماس با ما</a>
+                    </li>
+                    <li className={classes.menu__list}>
+                        <a href="#" className={classes.menu__link}>صفحه اول</a>
+                    </li>
+                    <li className={classes.menu__list}>
+                        <p className={classes.menu__logo}>فروشگاه</p>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
