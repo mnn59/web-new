e636e266d46db60c26fcb0b794a35ccb26217882
mahdi niknejad <m.niknejad@aut.ac.ir>
2021-07-14 14:47:56 -0700

    Implement Login[WIP]

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -1,18 +1,34 @@
 import React from 'react'
 import classes from './Login.module.css'
 import {Link} from "react-router-dom";
+import SearchButton from '../../components/UI/SearchButton/SearchButton'
+import Auxiliary from "../../hoc/Auxiliary";
 
 const login = () => {
     return (
-        <div className={classes.Login}>
-            <h1>Login Page</h1>
-            <span>
-                don't have an account ?
-                <Link to="/signup">Sign up</Link>
-            </span>
-            <br/>
-            <span>Go back <Link to="/">Home</Link></span>
-        </div>
+        <Auxiliary>
+            <div className={classes.grid_container}>
+                {/*<h1>Login Page</h1>*/}
+                {/*<span>*/}
+                {/*    don't have an account ?*/}
+                {/*    <Link to="/signup">Sign up</Link>*/}
+                {/*</span>*/}
+                {/*<br/>*/}
+                {/*<span>Go back <Link to="/">Home</Link></span>*/}
+                {/*<h1 className={classes.grid_item}></h1>*/}
+                {/*<h1 className={classes.grid_header}>salam</h1>*/}
+                <div className={classes.grid_item}>
+                    <input type="text" className={classes.email_input} placeholder="ایمیل خود را وارد کنید ..."/>
+                </div>
+                <div className={classes.label_item}>ایمیل</div>
+                <div className={classes.grid_item}>
+                    <input type="text" className={classes.password_input} placeholder="رمز عبور خود را وارد کنید ..."/>
+                </div>
+                <div className={classes.label_item}>رمز عبور</div>
+                <SearchButton value="ورود"/>
+            </div>
+        </Auxiliary>
+
     )
 }
 
