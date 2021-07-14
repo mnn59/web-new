0e4fae3e8056bcd14315fbe30590b5ec984b3573
mahdi niknejad <m.niknejad@aut.ac.ir>
2021-07-14 13:26:03 -0700

    Add Routes to App

--- src/App.js
+++ src/App.js
@@ -4,19 +4,36 @@ import Layout from './components/Layout/Layout'
 import LoginButton from './components/UI/LoginButton/LoginButton'
 import Header from './components/UI/Header/Header'
 import Footer from './components/UI/Footer/Footer'
+import HeroHeader from './components/UI/HeroHeader/HeroHeader'
+import ProductsSection from './components/UI/ProductsSection/ProductsSection'
+import {BrowserRouter as Router, Route, Redirect, Switch, Link} from 'react-router-dom'
+import Login from './containers/Login/Login'
+import Routes from './Routes'
+import Home from './containers/Home/Home'
 
 class App extends Component {
     render() {
         return (
-            <div className={classes.App}>
-                <Header />
-                {/*<Layout>*/}
-                {/*    <p>تست</p>*/}
-                {/*    <LoginButton value="ورود / ثبت نام"/>*/}
-                {/*</Layout>*/}
+            <Router>
+                <div className={classes.App}>
+                    {/*<Header/>*/}
+                    {/*<Layout>*/}
+                    {/*    <p>تست</p>*/}
+                    {/*    <LoginButton value="ورود / ثبت نام"/>*/}
+                    {/*</Layout>*/}
+                    {/*<HeroHeader/>*/}
+                    {/*<ProductsSection/>*/}
+                    {/*<Link to="/login">Go to login page</Link>*/}
+                    {/*<Footer/>*/}
 
-                <Footer />
-            </div>
+                    {/*<Home />*/}
+                    <Routes/>
+                </div>
+
+                {/*<Switch>*/}
+                {/*    <Route path="/login" exact component={Login} />*/}
+                {/*</Switch>*/}
+            </Router>
         );
     }
 }
