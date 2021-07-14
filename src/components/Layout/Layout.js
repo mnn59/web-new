aef764590a943f581bfd7092fe3611e0102ae006
mahdi niknejad <m.niknejad@aut.ac.ir>
2021-07-14 00:32:10 -0700

    Comment line

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -4,7 +4,7 @@ import classes from "./Layout.css"
 
 const layout = (props) => (
     <Aux>
-        <div>Toolbar sideDrawer, backdrop</div>
+        {/*<div>Toolbar sideDrawer, backdrop</div>*/}
         <main className={classes.Content}>
             {props.children}
         </main>
