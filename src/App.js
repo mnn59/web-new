caffc4d75338ced4f9c6f88451a4baf67fdc238b
mahdi niknejad <m.niknejad@aut.ac.ir>
2021-07-14 00:31:04 -0700

    Import Header and Footer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import classes from './App.module.css';
 import {Component} from 'react'
 import Layout from './components/Layout/Layout'
+import LoginButton from './components/UI/LoginButton/LoginButton'
+import Header from './components/UI/Header/Header'
+import Footer from './components/UI/Footer/Footer'
 
 class App extends Component {
     render() {
         return (
             <div className={classes.App}>
-                <Layout>
-                    <p>تست</p>
-                </Layout>
+                <Header />
+                {/*<Layout>*/}
+                {/*    <p>تست</p>*/}
+                {/*    <LoginButton value="ورود / ثبت نام"/>*/}
+                {/*</Layout>*/}
+
+                <Footer />
             </div>
         );
     }
