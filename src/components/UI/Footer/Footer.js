40c2797237ed6418e42bcf046968b4a1f9ec273d
mahdi niknejad <m.niknejad@aut.ac.ir>
2021-07-14 00:29:50 -0700

    Add Footer

---
+++ src/components/UI/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from './Footer.module.css'
+
+const footer = () => {
+    return (
+        <div className={classes.div__Footer}>
+            تمامی حقوق برای سعید و مهدی محفوظ است
+        </div>
+    )
+}
+
+export default footer
