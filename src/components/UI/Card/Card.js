bc6f28158dc5756cdae145c48d903d5039d8366c
mahdi niknejad <m.niknejad@aut.ac.ir>
2021-07-14 13:20:13 -0700

    Add card component

---
+++ src/components/UI/Card/Card.js
@@ -0,0 +1,21 @@
+import React from "react";
+import classes from './Card.module.css'
+
+const card = (props) => {
+    return (
+        <div className={classes.productCard}>
+            <img src={props.image} className={classes.productImage}/>
+            <h1 className={classes.productTitle}>{props.title}</h1>
+            <p className={classes.productSubTitle}>{props.subTitle}</p>
+            <hr className={classes.divider}/>
+            <div className={classes.productPriceSection}>
+                <p className={classes.productPrice}>
+                     تومان{props.price}
+                </p>
+                <button className={classes.BuyButton}>{props.buttonValue}</button>
+            </div>
+        </div>
+    )
+}
+
+export default card
