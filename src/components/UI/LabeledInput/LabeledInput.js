4b9c5ed15b9e6c646577e24b6fcf54cb19ee1853
mahdi niknejad <m.niknejad@aut.ac.ir>
2021-07-19 02:19:49 -0700

    Improve LabeledInput component in a generic manner

--- src/components/UI/LabeledInput/LabeledInput.js
+++ src/components/UI/LabeledInput/LabeledInput.js
@@ -4,13 +4,42 @@ import classNames from "classnames";
 
 const labeledInput = (props) => {
     const containerClass = classNames(classes.grid_container, props.className)
-    const labelClass =  classNames(classes.label_item, props.labelClassName)
+    // const labelClass = classNames(classes.label_item, props.labelClassName)
+
+    let InputElement = null
+
+    switch (props.elementType) {
+        case ('input'):
+            InputElement = <input
+                className={classes.input}
+                {...props.elementConfig}
+                value={props.value}
+                onChange={props.changed}
+            />
+            break
+        case ('textarea'):
+            InputElement = <textarea
+                className={classes.input}
+                {...props.elementConfig}
+                value={props.value}
+                onChange={props.changed}
+            />
+            break
+        default:
+            InputElement = <input
+                className={classes.input}
+                {...props.elementConfig}
+                value={props.value}
+                onChange={props.changed}
+            />
+    }
     return (
         <div className={containerClass}>
             <div className={classes.grid_item}>
-                <input type="text" className={classes.input} placeholder={props.placeholder}/>
+                {/*<input type={props.type} className={classes.input} placeholder={props.placeholder}/>*/}
+                {InputElement}
             </div>
-            <div className={labelClass}>{props.label}</div>
+            <div className={classes.label_item}>{props.label}</div>
         </div>
     )
 }
