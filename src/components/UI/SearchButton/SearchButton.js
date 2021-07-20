63120615714f9928f1591f1cbe461c637afbc44f
mahdi niknejad <m.niknejad@aut.ac.ir>
2021-07-20 13:03:29 -0700

    Add disabled feature for SearchButton

--- src/components/UI/SearchButton/SearchButton.js
+++ src/components/UI/SearchButton/SearchButton.js
@@ -6,7 +6,12 @@ class searchButton extends Component {
     render(){
         const btnClass = classNames(classes.searchBtn, this.props.className);
         return (
-            <button type="button" className={btnClass} onClick={this.props.onClick}>
+            <button
+                type={this.props.type}
+                className={btnClass}
+                onClick={this.props.onClick}
+                disabled={this.props.disabled}
+            >
                 {this.props.value}
             </button>
         )
