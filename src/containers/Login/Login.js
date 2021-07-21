012ad5cdee1be5639fdef727757c95f2af592a74
mahdi niknejad <m.niknejad@aut.ac.ir>
2021-07-21 02:51:21 -0700

    Remove some code for test[WIP]

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import classes from './Login.module.css'
 import {Link} from "react-router-dom";
 import LoginButton from '../../components/UI/SearchButton/SearchButton'
@@ -6,8 +6,12 @@ import Auxiliary from "../../hoc/Auxiliary";
 import Footer from '../../components/UI/Footer/Footer'
 import Header from '../../components/UI/Header/Header'
 import LabeledInput from '../../components/UI/LabeledInput/LabeledInput'
+import SortByBox from "../../components/UI/SortByBox/SortByBox";
 
 const login = () => {
+
+
+
     const inputHandler = () => {
         console.log('input validation')
     }
@@ -17,43 +21,49 @@ const login = () => {
         console.log(document.getElementById('email_input').value)
     }
     return (
+        // <Auxiliary>
+        //     <Header/>
+        //     <form className={classes.grid_container}>
+        //         <h1 className={classes.grid_header}>فروشگاه - ورود</h1>
+        //         <div className={classes.grid_item}>
+        //             <input
+        //                 type="text"
+        //                 className={classes.email_input}
+        //                 id="email_input"
+        //                 placeholder="ایمیل خود را وارد کنید ..."
+        //                 onClick={inputHandler}
+        //                 required
+        //             />
+        //         </div>
+        //
+        //         <div className={classes.label_item}>ایمیل</div>
+        //         <div className={classes.grid_item}>
+        //             <input
+        //                 type="password"
+        //                 className={classes.password_input}
+        //                 placeholder="رمز عبور خود را وارد کنید ..."
+        //                 required
+        //             />
+        //         </div>
+        //         <div className={classes.label_item}>رمز عبور</div>
+        //         <LoginButton
+        //             value="ورود"
+        //             className={classes.login_button}
+        //             onClick={LoginButtonHandler}
+        //             type="submit"
+        //         />
+        //     </form>
+        //     <Footer className={classes.footerHere}/>
+        // </Auxiliary>
+
         <Auxiliary>
             <Header />
-            <div className={classes.grid_container}>
-                {/*<h1>Login Page</h1>*/}
-                {/*<span>*/}
-                {/*    don't have an account ?*/}
-                {/*    <Link to="/signup">Sign up</Link>*/}
-                {/*</span>*/}
-                {/*<br/>*/}
-                {/*<span>Go back <Link to="/">Home</Link></span>*/}
-                {/*<h1 className={classes.grid_item}></h1>*/}
-                <h1 className={classes.grid_header}>فروشگاه - ورود</h1>
+            <form className={classes.grid_container}>
+                <strong className={classes.grid_header}>فروشگاه - ورود</strong>
+            </form>
 
-                <div className={classes.grid_item}>
-                    <input
-                        type="text"
-                        className={classes.email_input}
-                        id="email_input"
-                        placeholder="ایمیل خود را وارد کنید ..."
-                        onClick={inputHandler}
-                        required
-                    />
-                </div>
-                <div className={classes.label_item}>ایمیل</div>
-                {/*<LabeledInput placeholder="ایمیل خود را وارد کنید ..." label="ایمیل"/>*/}
-                <div className={classes.grid_item}>
-                    <input type="password" className={classes.password_input} placeholder="رمز عبور خود را وارد کنید ..."/>
-                </div>
-                <div className={classes.label_item}>رمز عبور</div>
-                {/*<LabeledInput placeholder="رمز عبور خود را وارد کنید ..." label="رمز عبور"/>*/}
-                <LoginButton
-                    value="ورود"
-                    className={classes.login_button}
-                    onClick={LoginButtonHandler}
-                    type="submit"
-                />
-            </div>
+            {/*test*/}
+            {/*<SortByBox/>*/}
             <Footer className={classes.footerHere}/>
         </Auxiliary>
 
