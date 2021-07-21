6c455a06d8445f54888bbc9bc173fc82297efacc
mahdi niknejad <m.niknejad@aut.ac.ir>
2021-07-21 02:52:50 -0700

    Add SortByBox component[WIP]

---
+++ src/components/UI/SortByBox/SortByBox.js
@@ -0,0 +1,31 @@
+import React from "react"
+import classes from "./SortByBox.css"
+
+
+// class does not appear in other classes like login
+
+const SortByBox = () => {
+    return (
+            <div className={classes.container}>
+                <ul className={classes.list} data-label="مرتب سازی بر اساس: ">
+                    <li className={classes.list_item}>
+                        <a href="#" className={classes.list_item_link}>
+                            سلام
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" className={classes.list_item_link}>
+                            خوبی
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" className={classes.list_item_link}>
+                            من
+                        </a>
+                    </li>
+                </ul>
+            </div>
+    )
+}
+
+export default SortByBox
