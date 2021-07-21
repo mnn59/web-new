6953fcd7452320cb2faae6022689f9a01b88950b
mahdi niknejad <m.niknejad@aut.ac.ir>
2021-07-21 02:52:16 -0700

    Edit ProductsSection

--- src/components/UI/ProductsSection/ProductsSection.js
+++ src/components/UI/ProductsSection/ProductsSection.js
@@ -2,31 +2,121 @@ import React from "react";
 import classes from './ProductsSection.module.css'
 import Card from '../Card/Card'
 import bag from '../../../assets/bag.png'
+import SortByBox from "../SortByBox/SortByBox";
 
 const productsSection = () => {
     return (
-        <div className={classes.productsSection}>
-            <Card
-                image={bag}
-                title="موس گیمینگ ریزر"
-                subTitle="دسته بندی یک"
-                price="۱۰۰۰۰ تومان"
-                buttonValue="خرید محصول"
-            />
-            <Card
-                image={bag}
-                title="موس گیمینگ ریزر"
-                subTitle="دسته بندی یک"
-                price="۱۰۰۰۰ تومان"
-                buttonValue="خرید محصول"
-            />
-            <Card
-                image={bag}
-                title="موس گیمینگ ریزر"
-                subTitle="دسته بندی یک"
-                price="۱۰۰۰۰ تومان"
-                buttonValue="خرید محصول"
-            />
+        <div className={classes.container}>
+            <div className={classes.productsSection}>
+                <Card
+                    image={bag}
+                    title="موس گیمینگ ریزر"
+                    subTitle="دسته بندی یک"
+                    price="۱۰۰۰۰"
+                    buttonValue="خرید محصول"
+                />
+                <Card
+                    image={bag}
+                    title="موس گیمینگ ریزر"
+                    subTitle="دسته بندی یک"
+                    price="۱۰۰۰۰"
+                    buttonValue="خرید محصول"
+                />
+                <Card
+                    image={bag}
+                    title="موس گیمینگ ریزر"
+                    subTitle="دسته بندی یک"
+                    price="۱۰۰۰۰"
+                    buttonValue="خرید محصول"
+                />
+                <Card
+                    image={bag}
+                    title="موس گیمینگ ریزر"
+                    subTitle="دسته بندی یک"
+                    price="۱۰۰۰۰"
+                    buttonValue="خرید محصول"
+                />
+                <Card
+                    image={bag}
+                    title="موس گیمینگ ریزر"
+                    subTitle="دسته بندی یک"
+                    price="۱۰۰۰۰"
+                    buttonValue="خرید محصول"
+                />
+                <Card
+                    image={bag}
+                    title="موس گیمینگ ریزر"
+                    subTitle="دسته بندی یک"
+                    price="۱۰۰۰۰"
+                    buttonValue="خرید محصول"
+                />
+                <Card
+                    image={bag}
+                    title="موس گیمینگ ریزر"
+                    subTitle="دسته بندی یک"
+                    price="۱۰۰۰۰"
+                    buttonValue="خرید محصول"
+                />
+                <Card
+                    image={bag}
+                    title="موس گیمینگ ریزر"
+                    subTitle="دسته بندی یک"
+                    price="۱۰۰۰۰"
+                    buttonValue="خرید محصول"
+                />
+                <Card
+                    image={bag}
+                    title="موس گیمینگ ریزر"
+                    subTitle="دسته بندی یک"
+                    price="۱۰۰۰۰"
+                    buttonValue="خرید محصول"
+                />
+                <Card
+                    image={bag}
+                    title="موس گیمینگ ریزر"
+                    subTitle="دسته بندی یک"
+                    price="۱۰۰۰۰"
+                    buttonValue="خرید محصول"
+                />
+                <Card
+                    image={bag}
+                    title="موس گیمینگ ریزر"
+                    subTitle="دسته بندی یک"
+                    price="۱۰۰۰۰"
+                    buttonValue="خرید محصول"
+                />
+                <Card
+                    image={bag}
+                    title="موس گیمینگ ریزر"
+                    subTitle="دسته بندی یک"
+                    price="۱۰۰۰۰"
+                    buttonValue="خرید محصول"
+                />
+                <Card
+                    image={bag}
+                    title="موس گیمینگ ریزر"
+                    subTitle="دسته بندی یک"
+                    price="۱۰۰۰۰"
+                    buttonValue="خرید محصول"
+                />
+
+
+                {/*<SortByBox />*/}
+            </div>
+            <div className={classes.sortByBox}>
+                <div className={classes.sortByCard}>
+                    here
+                </div>
+            </div>
+            <div className={classes.groupingBox}>
+                <div className={classes.groupCard}>
+                    here
+                </div>
+
+                <div className={classes.setPriceCard}>
+                    here
+                </div>
+            </div>
         </div>
     )
 }
